f3af026bed3efc7109a96cd323e6b00aff7449f4
Heyuan Liu <heyuan.liu@intel.com>
2022-09-29 15:53:27 +0800

    add engine

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env sh
-mvn exec:java -Dexec.mainClass=example.Service -Dconfig=config.properties
+export LD_LIBRARY_PATH=/usr/local/ssl/lib/:$LD_LIBRARY_PATH
+export OPENSSL_ENGINES=/usr/local/ssl/lib/engines-1.1/
+
+mvn exec:java -Dexec.mainClass=example.Service -Dconfig=config.properties -Dorg.wildfly.openssl.engine=qatengine
