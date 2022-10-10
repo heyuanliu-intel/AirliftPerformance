4496f38d8ffbb9cacdca9e5fa3a6029de0ae5de4
Heyuan Liu <heyuan.liu@intel.com>
2022-10-10 11:29:20 +0800

    add test script for short/long connection testing

--- run_wrk.sh
+++ run_wrk.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env sh
 
-wrk -t128 -c2000 -d120s  https://localhost:9300/v1/service
+if [ "${alive}" == "false" ]; then
+    echo "run short connection testing"
+    wrk -t128 -c2000 -d120s -H "Connection: Close" https://localhost:9300/v1/service
+else
+    echo "run long connection testing"
+    wrk -t128 -c2000 -d120s  https://localhost:9300/v1/service
+fi
+
